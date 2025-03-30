9660cd78c99f679b119ec2fb00b49b85b5ef8406
ActivatedOne <usualprovider@gmail.com>
2025-03-29 19:16:38 -0500

    Add data package entry point

---
+++ packages/data/src/index.ts
@@ -0,0 +1,14 @@
+// Export types
+export * from './types';
+
+// Export main data manager
+export * from './DataManager';
+
+// Export storage modules
+export * from './storage/DatabaseManager';
+
+// Export cache modules
+export * from './cache/CacheManager';
+
+// Export analysis modules
+export * from './analysis/DataAnalyzer';

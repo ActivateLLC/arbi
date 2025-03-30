621de38c661cdace13e1108fc71d413b269b2268
ActivatedOne <usualprovider@gmail.com>
2025-03-29 19:05:33 -0500

    Add voice-interface entry point

---
+++ packages/voice-interface/src/index.ts
@@ -0,0 +1,14 @@
+// Export types
+export * from './types';
+
+// Export main voice interface
+export * from './VoiceInterface';
+
+// Export recognition components
+export * from './recognition/WhisperRecognizer';
+
+// Export synthesis components
+export * from './synthesis/ElevenLabsSynthesizer';
+
+// Export wake word components
+export * from './wakeword/PorcupineDetector';

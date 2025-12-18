1b7a1bdcce589ac7cb458a53b678c0eb4fb07e60
Your Name <you@example.com>
2025-12-18 17:43:54 -0600

    Enable autonomous listing engine and Google Ads

---
+++ start_engine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Start the autonomous engine listing job
+curl -X POST https://arbi-production.up.railway.app/api/autonomous-control/start-listing \
+  -H "Content-Type: application/json" \
+  -d '{
+    "scanIntervalMinutes": 60,
+    "minScore": 75,
+    "minProfit": 20,
+    "minROI": 15,
+    "markupPercentage": 30,
+    "maxListingsPerRun": 10
+  '
